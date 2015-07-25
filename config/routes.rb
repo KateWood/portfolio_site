efc18e044cef5e357af455ddcc97ce553e2d52c5
Kate Wood <katewood611@gmail.com>
2015-07-25 14:59:27 -0400

    adds route to projects page

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   get '/resume' => "welcome#resume"
 
+  get '/projects' => "welcome#projects"
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
